66579657fae791ab0aac768baf21479d2edf455c
Benjamin Rothman <whiskman@gmail.com>
2021-08-12 20:02:56 -0400

    Added session controller with base functionality to create session, added SpamApiCore dependency

---
+++ Spam/SpamApi/Controllers/SessionController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SpamApi.Core;
+
+namespace SpamApi.Controllers
+{
+    public class SessionController : ApiController
+    {
+        [Route("api/Session/{userId}/{hostName}")]
+        [HttpGet]
+        public Guid Get(string userId, string hostName)
+        {
+            Session session = new Session(SessionEnvironment.Development, "", "", userId, hostName);
+            return session.Id;
+        }
+    }
+}
